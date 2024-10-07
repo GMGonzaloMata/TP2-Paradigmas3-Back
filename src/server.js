0a964ee6656f7fc7db82cfdd038bd8b2d36b7c5e
GMGonzaloMata <gonza.mata2003@gmail.com>
2024-10-07 00:04:38 -0300

    Ultima correcion PORT

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const InMemoryEventRepository = require('./InterfaceAdapters/InMemoryEventReposi
 const InMemorySculptorRepository = require('./InterfaceAdapters/InMemorySculptorRepository');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para procesar solicitudes en formato JSON
 app.use(bodyParser.json());
